02656268a57c3cc74768cd5b7816fb3be60780f1
yamadanaofumi <nvht0log@gmail.com>
2020-07-02 12:50:15 +0900

    fix-ml-test-score

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,7 @@
 module.exports = {
   someSidebar: {
     Queuery: ['introduction'],
+    MLOps: ['ml-test-score'],
     データマネジメント: ['data-management', 'data-management-get-started'],
     BigQuery: ['bigquery-access-controls', 'bigquery-access-controls-groups'],
   },
